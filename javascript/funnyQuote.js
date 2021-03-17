cedf13902e0f3b89c462b54b3f7276c639b0b7fc
liaWrOte <liaworkingonline@gmail.com>
2021-03-17 14:48:49 +0100

    code to clean

---
+++ javascript/funnyQuote.js
@@ -0,0 +1,23 @@
+
+let funnyQuote = {
+
+    init: function () {
+        funnyQuote.item = document.querySelector(".funny-quote");
+        funnyQuote.triggers = document.getElementsByClassName("quote-trigger");
+
+        for (let i = 0; i < funnyQuote.triggers.length; i++) {
+            funnyQuote.triggers[i].addEventListener("click",funnyQuote.showQuote);
+        };
+    },
+
+    showQuote: function () {
+        funnyQuote.item.classList.add("funny-quote-show");
+
+        setTimeout( () => {
+            funnyQuote.item.classList.remove("funny-quote-show");
+        }, 3500);
+    }
+
+};
+
+document.addEventListener("DOMContentLoaded", funnyQuote.init);
