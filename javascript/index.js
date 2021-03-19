74451e237da4fa8d66552fcab4b2aec8f8b510ad
liaWrOte <liaworkingonline@gmail.com>
2021-03-19 10:57:37 +0100

    see more arrow translate fix

--- javascript/index.js
+++ javascript/index.js
@@ -9,12 +9,12 @@ let conditionalLoading = {
         // loadings for desktop resolution
        if (window.innerWidth >= 1034) {
         stickyMenu.init(); // shadow menu on scroll
-        howItWorksEffect.init(); // hover effect
+        //howItWorksEffect.init(); // hover effect
         funnyQuote.init(); // funny quote appear on download action
        } 
 
        // loadings for tablet and smartphone resolutions
-       if (window.innerWidth < 1064) {
+       if (window.innerWidth < 1034) {
         hamburgerMenu.init(); // mobile menu behavior
         funnyQuote.init(); // funny quote appear on download action
        }     
