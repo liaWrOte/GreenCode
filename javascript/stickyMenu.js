9655c80d1c9df68d1b00f0ef3e4eb265a406b6e5
liaWrOte <liaworkingonline@gmail.com>
2021-03-19 08:57:25 +0100

    sticky menu js use class instead of css inside fix

--- javascript/stickyMenu.js
+++ javascript/stickyMenu.js
@@ -13,14 +13,10 @@ let stickyMenu = {
         stickyMenu.offsetY = this.pageYOffset;
 
         if ((stickyMenu.topPosition - stickyMenu.offsetY) < 1) {
-            stickyMenu.nav.style.boxShadow = "2px 7px 19px -5px rgba(0,0,0,0.21)";
-            stickyMenu.nav.style.webkitBoxShadow = "2px 7px 19px -5px rgba(0,0,0,0.21)";
-            stickyMenu.nav.style.mozBoxShadow = "2px 7px 19px -5px rgba(0,0,0,0.21)";
+            stickyMenu.nav.classList.add("banner-active");
         } 
         if (stickyMenu.offsetY < stickyMenu.nav.offsetHeight) {
-            stickyMenu.nav.style.boxShadow = "none";
-            stickyMenu.nav.style.webkitBoxShadow = "none";
-            stickyMenu.nav.style.mozBoxShadow = "none";
+            stickyMenu.nav.classList.remove("banner-active");
         }
     },
 };
